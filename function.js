27729a03050a378186ac07dfccf939eca723fdd8
NISHANTH <nishanthnishanth49592@>
2025-03-06 22:01:30 +0530

    Updated file(s)

--- function.js
+++ function.js
@@ -2,11 +2,9 @@ const http = require('http'); // Import the HTTP module
 
 // Create a server
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' }); // Set status and response type
-    res.end('Hello, welcome to my server!'); // Send response
+    res.writeHead(200); // Set status and response type
+    res.end('Hello, welcome to my servers!'); // Send response
 });
 
 // Start the server and listen on port 3000
-server.listen(2000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
+server.listen(2000);
